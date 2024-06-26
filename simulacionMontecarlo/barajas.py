69adfa54fb8b9ed526a566e9275b6b03b1a0cb9b
Fredy Yecid Castro Agray <fredy.castro@gmail.com>
2024-06-26 06:58:07 -0500

    calculo de posibilidad de pares

--- simulacionMontecarlo/barajas.py
+++ simulacionMontecarlo/barajas.py
@@ -1,6 +1,7 @@
 # simular una baraja de poker
 # mayuscula porque son consatantes PALOS
 import random 
+import collections
 
 
 PALOS = ['espada','corazon','rombos', 'trebol']
@@ -20,9 +21,40 @@ def obtener_mano(barajas, tamano_mano):
     mano = random.sample(barajas, tamano_mano)  # obtiene un valor diferente para evitar obtener el mismo valor sample
     return mano
 
-if __name__ == '__main__':    
+def main(tamano_mano, intenos):
     barajas = crear_baraja()
-    print(barajas)
-    mano = obtener_mano(barajas, 5)
-    print (mano)
+
+    manos=[]
+    for _ in range(intenos):
+        mano = obtener_mano(barajas, tamano_mano)
+        manos.append(mano)
+
+    pares = 0
+    for mano in manos:
+        valores =[]
+        for carta in mano:
+            valores.append(carta[1])
+          
+        counter = dict(collections.Counter(valores))
+        
+        # encontrar un par
+        for val in counter.values():
+            if val == 2:
+                pares += 1
+                break
+        probabilidad_par = pares /intenos
+    print(f'la probabilidad de obtener un par en una mano de {tamano_mano} barajas es : {probabilidad_par}')
+
+
+if __name__ == '__main__':    
+    tamano_mano = int(input('de Cuantas barajas sera la mano '))
+    intentos = int(input('Intentos para calcular la probabilidad: '))
+    main(tamano_mano, intentos) 
+
+    
+    
+    # barajas = crear_baraja()
+    # print(barajas)
+    # mano = obtener_mano(barajas, 5)
+    # print (mano)
     
